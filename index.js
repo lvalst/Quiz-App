0124796e27e70de5497f79b57f1066124199e485
Lauren Van Allen <lvalstine@gmail.com>
2019-12-03 10:22:09 -0600

    notes added to files to make lines intelligible, better names as well

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+//HEADING introPage
+// h1 section that has title of quiz, which hyperlinks back to beginning (does not change, no need jquery)-DONE
+
+//section 'quizScores' that keeps section stats ('score' and 'quesNumber') updated- needs access to 'store.js'; total number of ques and correct answers, score will tally when user clicks radio button that matches words in answer, quesnumber ++ until end of quiz. 
+//IN PROGRESS
+
+//section 'quizPortion' same styling, where ques, answers, result/restart page go
+//section 'startQuiz box' with intro/prompt and start quiz button- will be hidden when start quiz button clicked
+//IN PROGRESS
+
+//section for question, button w/ submit- changes to answer when clicked and button w/ nextquestion-
+//IN PROGRESS
+
+//section to show final score, changes to button to restart and links back to index.html
+//IN PROGRESS
+
+//BUTTONS all have same CSS, just diff words (called by class names)
+
+
+
+
+
+
+
 //stores quiz score and question number
 let score= 0;
 let questionNumber= 0;
@@ -9,7 +33,7 @@ function newQuestion() {
     } else {
         $('.startQuiz').hide();
         finalScore();
-        $('.questionNumber').text(5);
+        $('.quesNumber').text(5);
     }
 }
 
@@ -22,7 +46,7 @@ function updateScore() {
 //adds one to questionNumber and updates text of questionNumber on the quiz. at start, questionNumber 0, once hit begin button, adds one to indicate first question
 function updateQuesNum() {
     questionNumber++;
-    $('.questionNumber').text(questionNumber + 1);
+    $('.quesNumber').text(questionNumber + 1);
 }
 
 //if choose to retake quiz, resets score and questionNumber on the quiz to zero
@@ -30,7 +54,7 @@ function retakeTotal() {
     score = 0;
     questionNumber = 0;
     $('.score').text(0);
-    $('.questionNumber').text('-');
+    $('.quesNumber').text('-');
 }
 
 //starts quiz
@@ -38,7 +62,7 @@ function startQuiz() {
     $('.altBox').hide();
     $('.startQuiz').on('click', '#begin', function (event) {
         $('.startQuiz').hide();
-        $('.questionNumber').text(1);
+        $('.queNumber').text(1);
         $('.questionBox').show();
         $('.questionBox').prepend(generateQuestion());
     })

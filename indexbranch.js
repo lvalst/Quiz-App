ac260c7c0bcb81e56c3c89ae01bfcf92996742b9
Lauren Van Allen <lvalstine@gmail.com>
2019-12-09 19:32:39 -0600

    STORE moved into js branch

--- indexbranch.js
+++ indexbranch.js
@@ -1,3 +1,64 @@
+const STORE = [
+    {//1
+      question : 'How did Anton van Leeuwenhoek make an important contribution to the development of the cell theory?',
+      answers : [
+        'He theorized that the pattern component of the theory is that all are organisms are made of cells.',
+        'He theorized that the process compenent of the theory is that all cells come from pre-existing cells. ',
+        'He invented the first microscope and saw the first cell.',
+        'He invented more powerful microscopes and was the first to describe the variety of existing cells.'
+        ],
+      correctAnswer:
+        'He invented more powerful microscopes and was the first to describe the variety of existing cells.'
+    },
+    {//2
+      question:
+        'Which of these is not one of the five fundamental characteristics used to define life in an organism?',
+      answers: [
+        'Organisms may exclusively use hosts in order to reproduce and replicate.',
+        'Organisms are made up of membrane-bound cells, whcih regulates the passage of materials between the interior and exterior spaces.',
+        'Organisms must acquire and use energy to stay alive and reproduce.',
+        'Organisms are the product of evolution and a population of organisms will continue to evolve through time.'
+        ],
+      correctAnswer:
+        'Organisms may exclusively use hosts in order to reproduce and replicate.'
+    },
+    {//3
+      question:
+          'What does the term "evolution" mean?',
+      answers: [
+        'The strongest individuals produce the most offspring.',
+        'The characteristics of an individual change through the course of its life, in response to natural selection.',
+        'The characteristics of populations change through time.',
+        'The characteristics of species become more complex over time.'
+        ],
+      correctAnswer: 
+        'The characteristics of populations change through time.'
+    },
+    {//4
+      question: 'What does it mean to say that a characteristic of an oganism is heritable',
+      answers: [
+        'The characteristic evolves.',
+        'The characteristic can be passed on to offspring.',
+        'The characteristic is advantageous to the organism.',
+        'The characteristic does not vary in the population.'
+        ],
+      correctAnswer: 
+        'The characteristic can be passed on to offspring.'
+    },
+    {//5
+      question:
+        'In biology, to what does the term "fitness" refer?',
+      answers: [
+        'The degree of training and muscle mass an individual has, relative to others in the same population.',
+        'An individual\'s\ slimness, relative to others in the same population.',
+        'The longevity of a particular indivdual',
+        'An individual\'s\ ability to survive and reproduce.'
+        ],
+      correctAnswer:
+        'An individual\'s\ ability to survive and reproduce.'
+    }
+  ]
+
 //what does begin button do? 
 //how does button change to 'submit'
 //how does it take the information from STORE and feed it to the webpage in radio format
@@ -6,6 +67,7 @@
 //how does button change to continue
 //how does it show score and question number each page?
 //how does it show final scores and give option to restart
+
 
 //base quesNumber and score in quizScores
 let quesNumber = 0;
@@ -93,7 +155,7 @@ function makeQues(quesNumber) { //run this function based off of the quesNumber
 }
 
 function correctAns() {
-    $('.ansBox').prepend(
+    $('.ansBox').html(
         `<h2>Great job! You are correct</h2> 
         <p class = "sizeMe">${STORE[quesNumber].correctAnswer}</p>
         <p class = "sizeMe">Want to try the next one?</p>
@@ -126,9 +188,9 @@ function nextQues() {
 //need to prep page for actions
 function overallFun() {
     startQuiz();
+    makeQues();
     checkQues();
     nextQues();
-    makeQues();
 };
 
 $(overallFun);
